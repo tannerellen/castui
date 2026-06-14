69988f1947450c4843ffafd3f79cda3de8512c03
Tanner Ellen <tanner@seedcode.com>
2026-06-14 14:05:55 -0700

    chore: remove unnecessary types

--- types/types.d.ts
+++ types/types.d.ts
@@ -8,39 +8,6 @@ export type FieldMap = Map<
   { name: string; type: "string" | "number" | "boolean" }
 >;
 
-export type DeviceLink = {
-  ssid: string;
-  frequency: number;
-  rate: number;
-  channelWidth: string;
-  band: string;
-  channel: number;
-};
-
-export type Network = {
-  ssid: string;
-  security: string;
-  signal: number;
-  connected?: boolean;
-};
-
-export type KnownNetwork = {
-  ssid: string;
-  type: string;
-};
-
-export type NetworkDisplay = {
-  ssid: string;
-  security: string;
-  signal: string;
-  connected?: string;
-};
-
-export type NetworkLists = {
-  allNetworks: NetworkDisplay[];
-  knownNetworks: NetworkDisplay[];
-};
-
 export type ListTableDataItem = {
   [key: string]: string;
 };
